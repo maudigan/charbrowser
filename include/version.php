5a85eb31555a53905ddbdbff6a4be33ab61692cf
maudigan <mqemulator@gmail.com>
2022-01-17 14:47:12 -0600

    version stamp fix
    forgot to update the version stamp on last push

--- include/version.php
+++ include/version.php
@@ -28,7 +28,7 @@ if ( !defined('INCHARBROWSER') )
 }
 
 //version information
-$version      =    "v3.4.0"; 
+$version      =    "v3.4.1"; 
 
 //this file always gets replaced on a reinstall
 //this gets compared to the config file version stamp
